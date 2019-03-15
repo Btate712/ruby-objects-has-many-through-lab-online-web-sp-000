68b78894d77e4347a3dd58d371fb8f7027a26605
Robert Tate <btate712@gmail.com>
2019-03-15 13:00:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,5 @@
 class Patient
+  @@all = []
   attr_reader :name
 
   def initialize(name)
@@ -11,5 +12,9 @@ class Patient
 
   def appointments
     Appointment.all.select { |appointment| appointment.patient == self }
+  end
+
+  def self.all 
+    @@all 
   end
 end
