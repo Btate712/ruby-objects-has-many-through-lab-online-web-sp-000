74812ec3d7fc7d949a2f838e9558b726a6bb25c1
Robert Tate <btate712@gmail.com>
2019-03-15 12:55:59 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,6 +16,16 @@ class Doctor
     Appointment.all.select { |appointment| appointment.doctor == self }
   end
 
+  def patients
+    patients_array = []
+    Appointment.all.each do |appointment|
+      if appointment.doctor == self
+        patients_array << appointment.patient
+      end
+    end
+    patients_array
+  end
+
   def self.all
     @@all
   end
