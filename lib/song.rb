e65ccd874dc52a6cef10e6628131eec74c2d8e12
Robert Tate <btate712@gmail.com>
2019-03-15 12:40:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_reader :name, :genre
   attr_accessor :artist
-  
+
   @@all = []
 
   def initialize(name, artist, genre)
@@ -10,7 +10,7 @@ class Song
     @genre = genre
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
