0c31a3c68d9ed7a9bf2428d39bce609d6b9f68b6
Robert Tate <btate712@gmail.com>
2019-03-15 12:49:28 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,6 +8,10 @@ class Doctor
     @@all << self
   end
 
+  def new_appointment(date, patient, doctor)
+    appointment = Appointment.new(date, patient, doctor)
+  end
+
   def self.all
     @@all
   end
