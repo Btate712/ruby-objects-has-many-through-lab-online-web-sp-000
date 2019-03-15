685f3cdda3ce3cb0845d2c3ae4963b7fa95c3f4b
Robert Tate <btate712@gmail.com>
2019-03-15 12:45:01 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -12,6 +12,16 @@ class Genre
     Song.all.select { |song| song.genre == self }
   end
 
+  def artists
+    artist_array = []
+    Song.all.each do |song|
+      if song.genre == self
+        artist_array << song.artist
+      end
+    end
+    artist_array
+  end
+
   def self.all
     @@all
   end
